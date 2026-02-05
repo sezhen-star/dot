f336665f72f2259a153a2a36a5431e39ca7f7794
Page <3067822417@qq.com>
2026-02-05 11:19:27 +0800

    修改了动画

--- Assets/Script/Player/PlayerAnimation.cs
+++ Assets/Script/Player/PlayerAnimation.cs
@@ -21,8 +21,13 @@ public class PlayerAnimation : MonoBehaviour
     void Update()
     {
         float speed = Mathf.Abs(rb.velocity.x);
+        float yVel = rb.velocity.y;
 
         animator.SetFloat("speed", speed);
-        animator.SetBool("isGrounded", movement.IsGrounded);
+        animator.SetFloat("yVelocity", yVel);
+
+        // 动画层用的“落地判断”：必须真的没在往下掉
+        bool groundedForAnim = movement.IsGrounded && yVel <= 0.1f;
+        animator.SetBool("isGrounded", groundedForAnim);
     }
 }
